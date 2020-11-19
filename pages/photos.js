72300cad305ff592e184e35bf6af020f02aecdbb
unitehenry <unitehenry@gmail.com>
2020-11-19 17:36:41 -0500

    vsco link

--- pages/photos.js
+++ pages/photos.js
@@ -9,7 +9,7 @@ export async function getStaticProps() {
       try {
       const req = await axios({
         method: 'GET',
-        url: 'https://vsco.co/reidanna/gallery'
+        url: 'https://vsco.co/unitehenry/gallery'
       });
 
       const data = req['data']; 
